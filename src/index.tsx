3af2d5606a5feb23e4dab7e9bfc39e0432b43085
Juha Kujala <juha.kujala@anders.fi>
2021-11-04 12:29:48 +0200

    Add initial routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -13,7 +14,9 @@ import './assets/styles/main.scss';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
