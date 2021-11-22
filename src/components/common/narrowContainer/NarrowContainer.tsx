c634a0389cea3a2924cff54fdb64591b0ff03146
Juha Kujala <juha.kujala@anders.fi>
2021-11-22 16:03:00 +0200

    Remove NarrowContainer replaced by Container

--- src/components/common/narrowContainer/NarrowContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './NarrowContainer.module.scss';
-
-const NarrowContainer: React.FC = ({ children }) => <div className={styles['narrow-container']}>{children}</div>;
-
-export default NarrowContainer;
