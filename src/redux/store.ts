ef05669d70c6b061e094a8829ea4e2e9096aa576
Juha Kujala <juha.kujala@anders.fi>
2021-11-24 10:38:23 +0200

    Add initial query for fetching projects

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,18 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 import authReducer from './features/authSlice';
+import { projectApi } from './services/projectApi';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [projectApi.reducerPath]: projectApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(projectApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
