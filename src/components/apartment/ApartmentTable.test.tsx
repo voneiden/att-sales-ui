f677e84523508e897a510251e6f9f4e8f2b2934a
Juha Kujala <juha.kujala@anders.fi>
2021-12-07 09:12:26 +0200

    Add tests for ApartmentTable component

---
+++ src/components/apartment/ApartmentTable.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+
+import ApartmentTable from './ApartmentTable';
+import mockApartments from '../../mocks/apartments.json';
+import { Apartment } from '../../types';
+
+const apartments = mockApartments as Apartment[];
+
+describe('ApartmentTable', () => {
+  it('renders ApartmentTable component', () => {
+    const { container } = render(<ApartmentTable />);
+    const element = container.firstChild;
+    expect(element).toBeDefined();
+  });
+
+  it('renders table header elements', () => {
+    render(<ApartmentTable apartments={apartments} isLoading={false} isError={false} isSuccess={true} projectId={1} />);
+
+    expect(screen.getAllByText('components.apartment.ApartmentTable.apartment')).toBeDefined();
+    expect(screen.getAllByText('components.apartment.ApartmentTable.floor')).toBeDefined();
+    expect(screen.getAllByText('components.apartment.ApartmentTable.area')).toBeDefined();
+  });
+});
