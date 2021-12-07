a2cd78bd7dfc21200475f4af274b5946f51bb842
Juha Kujala <juha.kujala@anders.fi>
2021-12-07 08:58:06 +0200

    Update store.ts to support testing

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,28 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, ThunkAction, Action } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import type { PreloadedState } from '@reduxjs/toolkit';
 
 import authReducer from './features/authSlice';
 import { api } from './services/api';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    [api.reducerPath]: api.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+const rootReducer = combineReducers({
+  auth: authReducer,
+  [api.reducerPath]: api.reducer,
 });
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+    preloadedState,
+  });
+};
+
+export const store = setupStore();
 
 setupListeners(store.dispatch);
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
