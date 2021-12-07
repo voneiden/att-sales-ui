92d6c24a82178e414538dc1f1f6dece02740af39
Juha Kujala <juha.kujala@anders.fi>
2021-12-07 09:00:54 +0200

    Update ApartmentActions tests

--- src/components/apartment/ApartmentActions.test.tsx
+++ src/components/apartment/ApartmentActions.test.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
 import ApartmentActions from './ApartmentActions';
 
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: any) => key }),
-}));
+describe('ApartmentActions', () => {
+  it('renders ApartmentActions component', () => {
+    const { container } = render(<ApartmentActions />);
+    const element = container.firstChild;
+    expect(element).toBeDefined();
+  });
 
-test('renders ApartmentTable component', () => {
-  const { container } = render(<ApartmentActions />);
-  const element = container.firstChild;
-  expect(element).toBeDefined();
+  it('renders select element label', () => {
+    render(<ApartmentActions />);
+    expect(screen.getByText('components.apartment.ApartmentActions.show')).toBeDefined();
+  });
+
+  it('renders action button label', () => {
+    render(<ApartmentActions />);
+    expect(screen.getByText('components.apartment.ApartmentActions.action')).toBeDefined();
+  });
 });
