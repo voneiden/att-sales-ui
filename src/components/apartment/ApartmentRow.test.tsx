f98f121f6355577191310d50819b8909184e72b2
Juha Kujala <juha.kujala@anders.fi>
2021-12-07 09:04:00 +0200

    Update ApartmentRow tests

--- src/components/apartment/ApartmentRow.test.tsx
+++ src/components/apartment/ApartmentRow.test.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import ApartmentRow from './ApartmentRow';
-import { Apartment } from '../../types';
 import mockApartments from '../../mocks/apartments.json';
+import { Apartment } from '../../types';
 
 const mockApartment = mockApartments[0] as Apartment;
 
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: any) => key }),
-}));
+describe('ApartmentRow', () => {
+  it('renders ApartmentRow component', () => {
+    const { container } = render(<ApartmentRow apartment={mockApartment} />);
+    const element = container.firstChild;
+    expect(element).toBeDefined();
+  });
 
-test('renders ApartmentRow component', () => {
-  const { container } = render(<ApartmentRow apartment={mockApartment} />);
-  const element = container.firstChild;
-  expect(element).toBeDefined();
-});
+  it('renders apartment details', () => {
+    render(<ApartmentRow apartment={mockApartment} />);
 
-test('renders apartment details', () => {
-  render(<ApartmentRow apartment={mockApartment} />);
-
-  expect(screen.queryByText('A1')).not.toBeNull(); // apartment_number
-  expect(screen.queryByText('4h+kt+s')).not.toBeNull(); // apartment_structure
+    expect(screen.queryByText('A1')).not.toBeNull(); // apartment_number
+    expect(screen.queryByText('4h+kt+s')).not.toBeNull(); // apartment_structure
+  });
 });
