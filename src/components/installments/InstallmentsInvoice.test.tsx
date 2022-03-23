5ccd2f9b94baaf6461168334f2f00c5bd3c4517d
Juha Kujala <juha.kujala@anders.fi>
2022-03-23 13:39:48 +0200

    Add functionality to download installment invoices from the API

---
+++ src/components/installments/InstallmentsInvoice.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+
+import InstallmentsInvoice from './InstallmentsInvoice';
+import dummyReservation from '../../mocks/apartment_reservation.json';
+import dummyProject from '../../mocks/project.json';
+
+describe('InstallmentsInvoice', () => {
+  beforeEach(() => {
+    render(
+      <InstallmentsInvoice
+        reservationId={dummyReservation.id}
+        installments={dummyReservation.installments}
+        apartment={dummyProject.apartments[0]}
+        project={dummyProject}
+        handleCloseCallback={() => null}
+      />
+    );
+  });
+
+  it('renders project details', () => {
+    expect(screen.getByText('Taloyhtiö 30+')).toBeDefined();
+  });
+
+  it('renders apartment details', () => {
+    expect(screen.getByText('A1')).toBeDefined();
+    expect(screen.getByText('4h+kt+s', { exact: false })).toBeDefined();
+  });
+
+  it('renders table header elements', () => {
+    expect(screen.getByText('components.installments.InstallmentsInvoice.sum')).toBeDefined();
+    expect(screen.getByText('components.installments.InstallmentsInvoice.dueDate')).toBeDefined();
+  });
+
+  it('renders table tbody', () => {
+    expect(screen.getByDisplayValue('PAYMENT_1')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('PAYMENT_2')).toBeInTheDocument();
+    expect(screen.getAllByText('24.8.2019')).toBeDefined();
+  });
+
+  it('renders print and close buttons', () => {
+    expect(screen.getByText('components.installments.InstallmentsInvoice.print')).toBeDefined();
+    expect(screen.getByText('components.installments.InstallmentsInvoice.close')).toBeDefined();
+  });
+});
