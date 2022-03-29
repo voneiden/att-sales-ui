8eecef9ba71515fa9f289cedab9f3d9ea324a280
Juha Kujala <juha.kujala@anders.fi>
2022-03-29 13:26:16 +0300

    Fix tests for ApartmentRow and ApartmentTable

--- src/components/apartment/ApartmentRow.test.tsx
+++ src/components/apartment/ApartmentRow.test.tsx
@@ -1,29 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 
 import ApartmentRow from './ApartmentRow';
 import mockProject from '../../mocks/project.json';
 import { Apartment } from '../../types';
+import { renderWithProviders } from '../../test/test-utils';
 
 const mockApartment: Apartment = mockProject.apartments[0];
 
 describe('ApartmentRow', () => {
   it('renders ApartmentRow component', () => {
-    const { container } = render(
-      <BrowserRouter>
-        <ApartmentRow apartment={mockApartment} />
-      </BrowserRouter>
-    );
+    const { container } = renderWithProviders(<ApartmentRow apartment={mockApartment} />);
     const element = container.firstChild;
     expect(element).toBeDefined();
   });
 
   it('renders apartment details', () => {
-    render(
-      <BrowserRouter>
-        <ApartmentRow apartment={mockApartment} />
-      </BrowserRouter>
-    );
+    renderWithProviders(<ApartmentRow apartment={mockApartment} />);
 
     expect(screen.queryByText('A1')).not.toBeNull(); // apartment_number
     expect(screen.queryByText('4h+kt+s')).not.toBeNull(); // apartment_structure
--- src/components/apartment/ApartmentTable.test.tsx
+++ src/components/apartment/ApartmentTable.test.tsx
@@ -1,28 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 
 import ApartmentTable from './ApartmentTable';
 import mockProject from '../../mocks/project.json';
 import { Apartment } from '../../types';
+import { renderWithProviders } from '../../test/test-utils';
 
 const apartments: Apartment[] = mockProject.apartments;
 
 describe('ApartmentTable', () => {
   it('renders ApartmentTable component', () => {
-    const { container } = render(
-      <BrowserRouter>
-        <ApartmentTable />
-      </BrowserRouter>
-    );
+    const { container } = renderWithProviders(<ApartmentTable />);
     const element = container.firstChild;
     expect(element).toBeDefined();
   });
 
   it('renders table header elements', () => {
-    render(
-      <BrowserRouter>
-        <ApartmentTable apartments={apartments} isLoading={false} isError={false} isSuccess={true} projectId={1} />
-      </BrowserRouter>
+    renderWithProviders(
+      <ApartmentTable apartments={apartments} isLoading={false} isError={false} isSuccess={true} projectId={1} />
     );
 
     expect(screen.getAllByText('components.apartment.ApartmentTable.apartment')).toBeDefined();
