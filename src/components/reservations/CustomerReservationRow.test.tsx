7866c85196b8b9e7e7312bf64dd88f8fd99ca4c4
Juha Kujala <juha.kujala@anders.fi>
2022-05-17 14:41:00 +0300

    Fix test for CustomerReservationRow after changing mock data

--- src/components/reservations/CustomerReservationRow.test.tsx
+++ src/components/reservations/CustomerReservationRow.test.tsx
@@ -9,11 +9,11 @@ import dummyCustomer from '../../mocks/customer.json';
 describe('CustomerReservationRow', () => {
   it('renders the component', () => {
     const reservations = groupReservationsByProject(dummyCustomer.apartment_reservations);
-    const reservation = reservations[0][1];
+    const reservation = reservations[0][0];
 
     renderWithProviders(<CustomerReservationRow customer={dummyCustomer} reservation={reservation} />);
 
-    expect(screen.getByText('A10')).toBeDefined();
+    expect(screen.getByText('B16')).toBeDefined();
     expect(screen.getByText('components.reservations.CustomerReservationRow.priority', { exact: false })).toBeDefined();
     expect(screen.getByText('components.reservations.CustomerReservationRow.createDeedOfSale')).toBeDefined();
   });
