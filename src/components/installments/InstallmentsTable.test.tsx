2b9f052c013db0cba00023cc29a725587ffcc411
Juha Kujala <juha.kujala@anders.fi>
2022-05-25 08:32:46 +0300

    Fix tests for installments table

--- src/components/installments/InstallmentsTable.test.tsx
+++ src/components/installments/InstallmentsTable.test.tsx
@@ -1,26 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import InstallmentsTable from './InstallmentsTable';
+import { screen } from '@testing-library/react';
 
+import InstallmentsTable from './InstallmentsTable';
 import dummyInstallments from '../../mocks/apartment_installments.json';
 import { ApartmentInstallment } from '../../types';
+import { renderWithProviders } from '../../test/test-utils';
 
 const installments = dummyInstallments as ApartmentInstallment[];
 const emptyInstallments = [] as any; // One without installments
 
 describe('InstallmentsTable', () => {
   it('renders InstallmentsTable component', () => {
-    const { container } = render(<InstallmentsTable installments={emptyInstallments} />);
+    const { container } = renderWithProviders(<InstallmentsTable installments={emptyInstallments} />);
     const element = container.firstChild;
     expect(element).toBeDefined();
   });
 
   it('renders print button', () => {
-    render(<InstallmentsTable installments={emptyInstallments} />);
+    renderWithProviders(<InstallmentsTable installments={emptyInstallments} />);
     expect(screen.getByText('components.installments.InstallmentsTable.printBankTransfers')).toBeDefined();
   });
 
   it('renders table header', () => {
-    render(<InstallmentsTable installments={installments} />);
+    renderWithProviders(<InstallmentsTable installments={installments} />);
     expect(screen.getByText('components.installments.InstallmentsTable.installmentType')).toBeDefined();
     expect(screen.getByText('components.installments.InstallmentsTable.sum')).toBeDefined();
     expect(screen.getByText('components.installments.InstallmentsTable.dueDate')).toBeDefined();
@@ -30,7 +31,7 @@ describe('InstallmentsTable', () => {
   });
 
   it('renders table footer', () => {
-    render(<InstallmentsTable installments={installments} />);
+    renderWithProviders(<InstallmentsTable installments={installments} />);
     expect(screen.getByText('components.installments.InstallmentsTable.total')).toBeDefined();
   });
 });
