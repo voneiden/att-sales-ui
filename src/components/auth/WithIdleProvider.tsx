8abf66704d6f3d3ba877e9d5dac63bad6332e1c2
Juha Kujala <juha.kujala@anders.fi>
2022-09-14 15:11:56 +0300

    Fix cross tab issue from idle provider
    Add the onActive function to hide the inactivity modal from other tabs
    when the user is again active on any of the tabs

--- src/components/auth/WithIdleProvider.tsx
+++ src/components/auth/WithIdleProvider.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react';
 import { IdleTimerProvider } from 'react-idle-timer';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getClient } from '../../auth/oidc-react';
 import {
   hideAuthSessionExpiringModal,
   showAuthSessionExpiringModal,
 } from '../../redux/features/authSessionExpiringModalSlice';
+import { RootState } from '../../redux/store';
 
 export const TIMEOUT_MINUTES = process.env.REACT_APP_IDLE_TIMEOUT_MINUTES || '15';
 
 const IdleTimer: FC<unknown> = ({ children }) => {
   const client = getClient();
   const dispatch = useDispatch();
+  const isModalOpen = useSelector((state: RootState) => state.authSessionExpiringModal.isOpened);
 
   const onPrompt = (): void => {
     dispatch(showAuthSessionExpiringModal());
@@ -23,15 +25,23 @@ const IdleTimer: FC<unknown> = ({ children }) => {
     client.logout();
   };
 
+  const onActive = (): void => {
+    if (isModalOpen) {
+      dispatch(hideAuthSessionExpiringModal());
+    }
+  };
+
   return (
     <IdleTimerProvider
       timeout={1000 * 60 * parseInt(TIMEOUT_MINUTES)}
       onPrompt={onPrompt}
       onIdle={onIdle}
+      onActive={onActive}
       promptTimeout={1000 * 60}
       name="att-sales-ui-idle-timer"
       startOnMount
       crossTab
+      syncTimers={1000}
     >
       {children}
     </IdleTimerProvider>
